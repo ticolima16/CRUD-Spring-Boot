352a165769e2864fdd88e59ec6e15e17c47dfc5d
Tiago Barboza <ticolima16@gmail.com>
2024-10-08 08:14:01 -0300

    first commit

---
+++ backend/src/main/java/br/com/tiagobarboza/Aula/CRUD/AulaCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagobarboza.Aula.CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaCrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/tiagobarboza/Aula/CRUD/AulaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagobarboza.Aula.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
